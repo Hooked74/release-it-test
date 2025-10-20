77ba918277862e86df66c8f91fb7d0a97a4041cb
Новиков Игорь <novikovio74@gmail.com>
2025-10-20 09:08:13 +0500

    feat: add env

--- scripts/build.js
+++ scripts/build.js
@@ -6,6 +6,8 @@ const projectDir = process.cwd();
 const srcDir = resolve(projectDir, "src");
 const distDir = resolve(projectDir, "dist");
 
+console.log("ENVIRONMENT:", process.env.ENVIRONMENT);
+
 if (!existsSync(srcDir)) {
   console.error("No src directory found for", projectDir);
   process.exit(1);
